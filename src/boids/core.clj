b7f3af6261ac027431efbeb318f37861091501f6
Gabe Truitt <gabe.truitt@gmail.com>
2021-09-06 16:40:14 -0400

    boids with random trajectories

--- src/boids/core.clj
+++ src/boids/core.clj
@@ -1,27 +1,75 @@
 (ns boids.core
-  (:require [quil.core :as q]))
+  (:import (java.util UUID))
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(def frame-rate 30)
+(def background-shade 63)
+(def num-boids 256)
+(def boid-shade 191)
+(def boid-stroke-shade 0)
+(def boid-stroke-weight 2)
+(def boid-speed 2)
+(def boid-size 8)
+(def field-size-x 1024)
+(def field-size-y 633)
+(def smoothing-level 2)
+
+(defn random-boid
+  []
+  {:x (q/random (q/width))
+   :y (q/random (q/height))
+   :heading (q/random q/TWO-PI)
+   :id (UUID/randomUUID)})
 
 (defn setup
   []
-  (q/frame-rate 10)
-  (q/background 200))
+  (q/frame-rate frame-rate)
+  (q/background background-shade)
+  (q/fill boid-shade)
+  (q/stroke boid-stroke-shade)
+  (q/stroke-weight boid-stroke-weight)
+  {:boids (take num-boids (repeatedly random-boid))})
 
-(defn draw
-  []
-  (q/stroke (q/random 255))
-  (q/stroke-weight (q/random 10))
-  (q/fill (q/random 255))
-  (let [diam (q/random 100)
-        x    (q/random (q/width))
-        y    (q/random (q/height))]
-    (q/ellipse x y diam diam)))
+(defn wrap-boundary
+ [value max-value]
+ (if (> value max-value)
+   (- value max-value)
+   value))
+
+(defn boid-heading
+ [state boid]
+ (:heading boid))
+
+(defn move-boid
+  [state boid]
+  (let [new-heading (boid-heading state boid)
+        new-x (+ (:x boid) (* boid-speed (q/cos new-heading)))
+        new-y (+ (:y boid) (* boid-speed (q/sin new-heading)))]
+    {:x (wrap-boundary new-x field-size-x)
+     :y (wrap-boundary new-y field-size-y)
+     :heading new-heading
+     :id (:id boid)}))
+
+(defn update-state
+  [state]
+  (update state :boids #(map (partial move-boid state) %)))
+
+(defn draw-state
+  [state]
+  (q/background background-shade)
+  (doseq [b (:boids state)]
+    (q/ellipse (:x b) (:y b) boid-size boid-size))
+  state)
 
 (q/defsketch sketch
   :title "grey circles"
-  :settings #(q/smooth 2)
+  :size [field-size-x field-size-y]
+  :settings #(q/smooth smoothing-level)
   :setup setup
-  :draw draw
-  :size [809 500])
+  :update update-state
+  :draw draw-state
+  :middleware [m/fun-mode])
 
 (defn -main
   [& _args])
